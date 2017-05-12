f03d2500928f22d459a4057626e9e49d345b6801
subes <gsubes@gmail.com>
2017-05-12 22:35:26 +0200

    script file better solution

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-jep/src/main/java/de/invesdwin/context/python/runtime/jep/JepScriptTaskEnginePython.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-jep/src/main/java/de/invesdwin/context/python/runtime/jep/JepScriptTaskEnginePython.java
@@ -1,26 +1,46 @@
 package de.invesdwin.context.python.runtime.jep;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
 import org.apache.commons.io.FileUtils;
 
+import de.invesdwin.context.ContextProperties;
 import de.invesdwin.context.integration.script.IScriptTaskEngine;
 import de.invesdwin.context.r.runtime.contract.IScriptTaskRunnerPython;
+import de.invesdwin.util.lang.UniqueNameGenerator;
 import jep.Jep;
 
 @NotThreadSafe
 public class JepScriptTaskEnginePython implements IScriptTaskEngine {
 
+    private static final UniqueNameGenerator UNIQUE_NAME_GENERATOR = new UniqueNameGenerator() {
+        @Override
+        protected long getInitialValue() {
+            return 1;
+        }
+    };
+    private static final File FOLDER = new File(ContextProperties.TEMP_DIRECTORY,
+            JepScriptTaskEnginePython.class.getSimpleName());
+
     private Jep jep;
     private final JepScriptTaskInputsPython inputs;
     private final JepScriptTaskResultsPython results;
+    private final File scriptFile;
 
     public JepScriptTaskEnginePython(final Jep jep) {
         this.jep = jep;
         this.inputs = new JepScriptTaskInputsPython(this);
         this.results = new JepScriptTaskResultsPython(this);
+        try {
+            FileUtils.forceMkdir(FOLDER);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.scriptFile = new File(FOLDER, UNIQUE_NAME_GENERATOR.get("script") + ".py");
     }
 
     /**
@@ -29,10 +49,8 @@ public class JepScriptTaskEnginePython implements IScriptTaskEngine {
     @Override
     public void eval(final String expression) {
         try {
-            //TODO: try to find a better alternative here
-            final File script = File.createTempFile("pythonScript", ".py");
-            FileUtils.writeStringToFile(script, expression);
-            jep.runScript(script.getAbsolutePath());
+            FileUtils.writeStringToFile(scriptFile, expression, Charset.defaultCharset());
+            jep.runScript(scriptFile.getAbsolutePath());
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
@@ -51,6 +69,7 @@ public class JepScriptTaskEnginePython implements IScriptTaskEngine {
     @Override
     public void close() {
         eval(IScriptTaskRunnerPython.CLEANUP_SCRIPT);
+        FileUtils.deleteQuietly(scriptFile);
         jep = null;
     }
 
