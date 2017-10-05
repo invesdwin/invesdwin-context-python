8955a26cae631642dcc7956ef61a3d69cbb9ff95
subes <gsubes@gmail.com>
2017-10-06 00:35:36 +0200

    checkstyle

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-py4j/src/main/java/de/invesdwin/context/python/runtime/py4j/pool/internal/IPy4jInterpreter.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-py4j/src/main/java/de/invesdwin/context/python/runtime/py4j/pool/internal/IPy4jInterpreter.java
@@ -2,11 +2,11 @@ package de.invesdwin.context.python.runtime.py4j.pool.internal;
 
 public interface IPy4jInterpreter {
 
-    Object get(final String variable);
+    Object get(String variable);
 
     void eval(String expression);
 
-    void put(final String variable, final Object value);
+    void put(String variable, Object value);
 
     void cleanup();
 
