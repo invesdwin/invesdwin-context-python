e249f429adf874644528b2e1a25940da989e1acd
subes <gsubes@gmail.com>
2017-10-11 00:41:25 +0200

    back to junit 4 since junit 5 breaks aspectj

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-jython/src/test/java/de/invesdwin/context/python/runtime/jython/JythonScriptTaskRunnerPythonTest.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-jython/src/test/java/de/invesdwin/context/python/runtime/jython/JythonScriptTaskRunnerPythonTest.java
@@ -3,7 +3,7 @@ package de.invesdwin.context.python.runtime.jython;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.inject.Inject;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import de.invesdwin.context.python.runtime.contract.InputsAndResultsTests;
 import de.invesdwin.context.test.ATest;
--- invesdwin-context-python-parent/invesdwin-context-python-runtime-py4j/src/test/java/de/invesdwin/context/python/runtime/py4j/Py4jScriptTaskRunnerPythonTest.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-py4j/src/test/java/de/invesdwin/context/python/runtime/py4j/Py4jScriptTaskRunnerPythonTest.java
@@ -3,7 +3,7 @@ package de.invesdwin.context.python.runtime.py4j;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.inject.Inject;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import de.invesdwin.context.python.runtime.contract.InputsAndResultsTests;
 import de.invesdwin.context.test.ATest;
