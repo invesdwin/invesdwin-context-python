864d4775d3ecc48d4f95e85a6a3e55f2503affa2
Edwin Stang <gsubes@gmail.com>
2022-01-01 19:05:41 +0100

    use a fastcallablecontext

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-libpythonclj/src/main/java/de/invesdwin/context/python/runtime/libpythonclj/internal/UncheckedPythonEngineWrapper.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-libpythonclj/src/main/java/de/invesdwin/context/python/runtime/libpythonclj/internal/UncheckedPythonEngineWrapper.java
@@ -11,16 +11,15 @@ import de.invesdwin.context.python.runtime.contract.IScriptTaskRunnerPython;
 import de.invesdwin.context.python.runtime.libpythonclj.LibpythoncljProperties;
 import de.invesdwin.context.python.runtime.libpythonclj.LibpythoncljScriptTaskEnginePython;
 import de.invesdwin.util.concurrent.lock.ILock;
-import de.invesdwin.util.lang.UniqueNameGenerator;
 
 @NotThreadSafe
 public final class UncheckedPythonEngineWrapper implements IPythonEngineWrapper {
 
     public static final UncheckedPythonEngineWrapper INSTANCE = new UncheckedPythonEngineWrapper();
 
-    private static final UniqueNameGenerator FUNCTIONS_NAMES = new UniqueNameGenerator();
     private Map<Object, Object> globals;
     private AutoCloseable fastCallable;
+    private Object fastCallableContext;
 
     private UncheckedPythonEngineWrapper() {
     }
@@ -32,10 +31,10 @@ public final class UncheckedPythonEngineWrapper implements IPythonEngineWrapper 
         libpython_clj2.java_api.initialize(initParams);
 
         final String fastCallableName = "__fastCallable__";
-        final Map<?, ?> mainModule = libpython_clj2.java_api
-                .runString("def " + fastCallableName + "(script):\n\texec(script, globals(), globals())");
+        final Map<?, ?> mainModule = libpython_clj2.java_api.runString(fastCallableName + " = exec");
         this.globals = (Map<Object, Object>) mainModule.get("globals");
         this.fastCallable = libpython_clj2.java_api.makeFastcallable(globals.get(fastCallableName));
+        this.fastCallableContext = libpython_clj2.java_api.allocateFastcallContext();
 
         final LibpythoncljScriptTaskEnginePython engine = new LibpythoncljScriptTaskEnginePython(this);
         engine.eval(new ClassPathResource(UncheckedPythonEngineWrapper.class.getSimpleName() + ".py",
@@ -59,7 +58,7 @@ public final class UncheckedPythonEngineWrapper implements IPythonEngineWrapper 
             libpython_clj2.java_api.runString(expression);
         } else {
             globals.put("__script__", expression);
-            libpython_clj2.java_api.call(fastCallable, "__script__");
+            libpython_clj2.java_api.fastcall(fastCallableContext, fastCallable, "__script__");
         }
     }
 
