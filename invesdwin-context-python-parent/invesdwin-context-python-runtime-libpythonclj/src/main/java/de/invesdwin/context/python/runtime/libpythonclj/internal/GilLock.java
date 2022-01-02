d73bf24ce053aa415542f3763c6ba73b50263ed0
Edwin Stang <gsubes@gmail.com>
2022-01-02 01:57:40 +0100

    faster reentrancy check

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-libpythonclj/src/main/java/de/invesdwin/context/python/runtime/libpythonclj/internal/GilLock.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-libpythonclj/src/main/java/de/invesdwin/context/python/runtime/libpythonclj/internal/GilLock.java
@@ -5,13 +5,13 @@ import javax.annotation.concurrent.NotThreadSafe;
 import org.apache.commons.lang3.mutable.MutableInt;
 
 import de.invesdwin.util.concurrent.lock.ASimpleLock;
-import de.invesdwin.util.concurrent.lock.ILock;
+import de.invesdwin.util.concurrent.lock.IReentrantLock;
 import de.invesdwin.util.concurrent.lock.Locks;
 
 @NotThreadSafe
 public final class GilLock extends ASimpleLock {
 
-    private static final ILock MAIN_LOCK = Locks.newReentrantLock("libpython-clj_MAIN");
+    private static final IReentrantLock MAIN_LOCK = Locks.newReentrantLock("libpython-clj_MAIN");
     private static final String GIL_LOCK_NAME = "libpython-clj_GIL";
 
     private final MutableInt lockedCount = new MutableInt();
@@ -22,8 +22,8 @@ public final class GilLock extends ASimpleLock {
 
     @Override
     public void lock() {
-        MAIN_LOCK.lock();
         if (lockedCount.incrementAndGet() == 1) {
+            MAIN_LOCK.lock();
             key = libpython_clj2.java_api.lockGIL();
         }
     }
@@ -32,8 +32,8 @@ public final class GilLock extends ASimpleLock {
     public void unlock() {
         if (lockedCount.decrementAndGet() == 0) {
             libpython_clj2.java_api.unlockGIL(key);
+            MAIN_LOCK.unlock();
         }
-        MAIN_LOCK.unlock();
     }
 
     @Override
