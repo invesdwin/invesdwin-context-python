55e793ea2984e3fe0e1a410220e3162cada7e966
Edwin Stang <gsubes@gmail.com>
2022-01-10 18:05:03 +0100

    enable all tests again

--- invesdwin-context-python-parent/invesdwin-context-python-runtime-contract/src/test/java/de/invesdwin/context/python/runtime/contract/InputsAndResultsTests.java
+++ invesdwin-context-python-parent/invesdwin-context-python-runtime-contract/src/test/java/de/invesdwin/context/python/runtime/contract/InputsAndResultsTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import de.invesdwin.context.python.runtime.contract.hello.HelloWorldScript;
 import de.invesdwin.util.concurrent.Executors;
 import de.invesdwin.util.concurrent.WrappedExecutorService;
 import de.invesdwin.util.concurrent.future.Futures;
@@ -19,22 +20,22 @@ public class InputsAndResultsTests {
     }
 
     public void test() {
-        //        new HelloWorldScript(runner).testHelloWorld();
-        //        new InputsAndResultsTestByte(runner).testByte();
-        //        new InputsAndResultsTestFloat(runner).testFloat();
-        //        new InputsAndResultsTestDouble(runner).testDouble();
-        //        new InputsAndResultsTestDecimal(runner).testDecimal();
-        //        new InputsAndResultsTestPercent(runner).testPercent();
-        //        new InputsAndResultsTestShort(runner).testShort();
-        //        new InputsAndResultsTestInteger(runner).testInteger();
-        //        new InputsAndResultsTestLong(runner).testLong();
-        //        new InputsAndResultsTestCharacter(runner).testCharacter();
+        new HelloWorldScript(runner).testHelloWorld();
+        new InputsAndResultsTestByte(runner).testByte();
+        new InputsAndResultsTestFloat(runner).testFloat();
+        new InputsAndResultsTestDouble(runner).testDouble();
+        new InputsAndResultsTestDecimal(runner).testDecimal();
+        new InputsAndResultsTestPercent(runner).testPercent();
+        new InputsAndResultsTestShort(runner).testShort();
+        new InputsAndResultsTestInteger(runner).testInteger();
+        new InputsAndResultsTestLong(runner).testLong();
+        new InputsAndResultsTestCharacter(runner).testCharacter();
         new InputsAndResultsTestString(runner).testString();
-        //        new InputsAndResultsTestBoolean(runner).testBoolean();
-        //        new InputsAndResultsTestNull(runner).testNull();
-        //        new InputsAndResultsTestNullPutGet(runner).testNullPutGet();
-        //        new InputsAndResultsTestEmpty(runner).testEmpty();
-        //        new InputsAndResultsTestEmptyMatrixValue(runner).testEmptyMatrixValue();
+        new InputsAndResultsTestBoolean(runner).testBoolean();
+        new InputsAndResultsTestNull(runner).testNull();
+        new InputsAndResultsTestNullPutGet(runner).testNullPutGet();
+        new InputsAndResultsTestEmpty(runner).testEmpty();
+        new InputsAndResultsTestEmptyMatrixValue(runner).testEmptyMatrixValue();
     }
 
     public void testParallel() {
